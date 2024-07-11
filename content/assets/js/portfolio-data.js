985cfded050e36e0ead0d9b7e23d6688f9f386b5
zeeshan ahmad <zeeshanaslam321@outlook.com>
2024-07-12 03:52:15 +0500

    changes

---
+++ content/assets/js/portfolio-data.js
@@ -0,0 +1,131 @@
+﻿
+
+
+var portfolioData = [
+    {
+        projectName: 'TheDocApp',
+        projectCategory: 'Full Stack Developer',
+        projectClient: 'USA Client',
+        projectUrl: 'www.thedocapp.com',
+        projectAbsoluteUrl: 'https://thedocapp.net/',
+        projectInfoTitle: 'TheDocApp Information',
+        projectInfo: 'The DocApp allows physicians to access patient profiles in real time, and assess progress at a glance. Physician-patient communication becomes faster, easier, and seamless. Workflow, time management, and documentation become efficient, eliminating time-wasting searches for missing information. The DocApp enables physicians to bring information for individual patients and for their practice as a whole together on one device. Desktops, iPads, laptops, or even personal devices can be used to securely access The DocApp.',
+        projectImages: [
+            'v1655926603/webdoc/img/portfolio/docapp/docapp-1.png', 'v1655926605/webdoc/img/portfolio/docapp/docapp-2.png',
+            'v1655926613/webdoc/img/portfolio/docapp/docapp-3.png', 'v1655926608/webdoc/img/portfolio/docapp/docapp-4.png',
+            'v1655926610/webdoc/img/portfolio/docapp/docapp-5.png'
+        ]
+    },
+    {
+        projectName: 'Invoiced',
+        projectCategory: 'API Developer',
+        projectClient: 'USA Client',
+        projectUrl: 'www.invoiced.com',
+        projectAbsoluteUrl: 'https://www.invoiced.com/',
+        projectInfoTitle: 'Invoiced Information',
+        projectInfo: 'The Invoiced Accounts Receivable Cloud lets you program your entire accounts receivable operation, complete with dashboards, integrated reporting and specialized tools for efficient, effective Accounts Receivable management.',
+        projectImages: [
+            'v1655926615/webdoc/img/portfolio/invoiced/invoiced-1.png', 'v1655926614/webdoc/img/portfolio/invoiced/invoiced-2.png',
+            'v1655926615/webdoc/img/portfolio/invoiced/invoiced-3.png', 'v1655926615/webdoc/img/portfolio/invoiced/invoiced-4.png',
+            'v1655926616/webdoc/img/portfolio/invoiced/invoiced-5.png'
+        ]
+    },
+    {
+        projectName: 'Psyter',
+        projectCategory: 'Full Stack Developer',
+        projectClient: 'Saudi Arabia Client',
+        projectUrl: 'www.psyter.com',
+        projectAbsoluteUrl: 'https://www.psyter.com/',
+        projectInfoTitle: 'Psyter Information',
+        projectInfo: 'Psyter designe the Site to be the complete electronic solution for those who feel sad, anxious, or suffer from life problems.  This electronic platform contains accurate assessment methods to find out what you may suffer from and provides you  with treatment options to solve those problems.',
+        projectImages: [
+            'v1655926622/webdoc/img/portfolio/psyter/psyter-1.png', 'v1655926620/webdoc/img/portfolio/psyter/psyter-2.png',
+            'v1655926620/webdoc/img/portfolio/psyter/psyter-3.png', 'v1655926619/webdoc/img/portfolio/psyter/psyter-4.png',
+            'v1655926621/webdoc/img/portfolio/psyter/psyter-5.png'
+        ]
+    },
+    {
+        projectName: 'MealPrep',
+        projectCategory: 'Full Stack Developer',
+        projectClient: 'US Client',
+        projectUrl: 'www.meal-prep.azurewebsites.net',
+        projectAbsoluteUrl: 'https://meal-prep.azurewebsites.net/',
+        projectInfoTitle: 'Meal Prep Information',
+        projectInfo: 'The Recipe application is used by a meal prep company to manage recipes, ingredients, and cook procedures. Key functions are Store recipes, Generate a cooking list and prep list and Generate a shopping list. Orders are received in advance.From the orders, kitchen staff can create order summaries, shopping lists, and prep lists.A Prep list is a set of tasks kitchen staff need to complete to create the dishes.',
+        projectImages: [
+            'v1655926615/webdoc/img/portfolio/mealprep/mealprep-6.png', 'v1655926616/webdoc/img/portfolio/mealprep/mealprep-2.png',
+            'v1655926616/webdoc/img/portfolio/mealprep/mealprep-3.png', 'v1655926616/webdoc/img/portfolio/mealprep/mealprep-4.png',
+            'v1655926617/webdoc/img/portfolio/mealprep/mealprep-5.png'
+        ]
+    },
+    {
+        projectName: 'PapaJohns',
+        projectCategory: 'API Developer',
+        projectClient: 'US Client',
+        projectUrl: 'www.papajohns.com.pk',
+        projectAbsoluteUrl: 'https://www.papajohns.com.pk/',
+        projectInfoTitle: 'PapaJohns Information',
+        projectInfo: 'Papa Johns International, Inc., is an American pizza restaurant chain.It is the fourth largest pizza delivery restaurant chain in the United States, with headquarters in Louisville, Kentucky and Atlanta, Georgia metropolitan areas.',
+        projectImages: [
+            'v1655926618/webdoc/img/portfolio/PapaJohns/papajohns-1.png', 'v1655926618/webdoc/img/portfolio/PapaJohns/papajohns-2.png',
+            'v1655926620/webdoc/img/portfolio/PapaJohns/papajohns-3.png', 'v1655926617/webdoc/img/portfolio/PapaJohns/papajohns-4.png',
+            'v1655926623/webdoc/img/portfolio/PapaJohns/papajohns-5.png'
+        ]
+    },
+    {
+        projectName: 'eSchool',
+        projectCategory: 'Backend Developer',
+        projectClient: 'Saudi Arabia Client',
+        projectUrl: 'www.eschool.sa',
+        projectAbsoluteUrl: 'http://www.eschool.sa/',
+        projectInfoTitle: 'eShool Information',
+        projectInfo: 'Students always come first. Our mission is to improve the education of students. Our focus is on the whole student - we build tools that ultimately help enrich both their intellectual and emotional growth that why we link education not only to knowledge but to skills and attitude as well. We do this by empowering the greatest learning resource that students have at school: educators.',
+        projectImages: [
+            'v1655926609/webdoc/img/portfolio/eSchool/eSchool-1.png', 'v1655926609/webdoc/img/portfolio/eSchool/eSchool-2.png',
+            'v1655926611/webdoc/img/portfolio/eSchool/eSchool-3.png', 'v1655926611/webdoc/img/portfolio/eSchool/eSchool-4.png',
+            'v1655926611/webdoc/img/portfolio/eSchool/eSchool-5.png'
+        ]
+    },
+    {
+        projectName: 'eVisit',
+        projectCategory: 'FullStack Developer',
+        projectClient: 'Europe Client',
+        projectUrl: 'www.evisit.com',
+        projectAbsoluteUrl: 'https://www.evisit.com/',
+        projectInfoTitle: 'eShool Information',
+        projectInfo: 'Having too many telehealth products is a management and reporting nightmare. eVisit is the one telehealth platform that includes the patient visit and everything around it — scheduling, intake, waiting room management, and discharge.',
+        projectImages: [
+            'v1655926613/webdoc/img/portfolio/eVisit/eVisit-1.png', 'v1655926612/webdoc/img/portfolio/eVisit/eVisit-2.png',
+            'v1655926613/webdoc/img/portfolio/eVisit/eVisit-3.png', 'v1655926615/webdoc/img/portfolio/eVisit/eVisit-4.png',
+            'v1655926615/webdoc/img/portfolio/eVisit/eVisit-5.png'
+        ]
+    },
+    {
+        projectName: 'TheParkingSpot',
+        projectCategory: 'Backend Developer',
+        projectClient: 'US Client',
+        projectUrl: 'www.theparkingspot.com',
+        projectAbsoluteUrl: 'https://www.theparkingspot.com/',
+        projectInfoTitle: 'TheParkingSpot Information',
+        projectInfo: 'The Parking Spots airport parking lots offer convenient and friendly service.In addition to always being open, well lit, and fenced, many facilities offer valet services and the choice of covered or open- air parking.Expect the best for short and long - term airport parking.',
+        projectImages: [
+            'v1655926606/webdoc/img/portfolio/theparkingspot/theparkingspot-1.png', 'v1655926621/webdoc/img/portfolio/theparkingspot/theparkingspot-2.png',
+            'v1655926633/webdoc/img/portfolio/theparkingspot/theparkingspot-3.png', 'v1655926629/webdoc/img/portfolio/theparkingspot/theparkingspot-4.png',
+            'v1655926621/webdoc/img/portfolio/theparkingspot/theparkingspot-5.png'
+        ]
+    },
+    {
+        projectName: 'vtindustries',
+        projectCategory: 'Backend Developer',
+        projectClient: 'US Client',
+        projectUrl: 'www.vtindustries.com',
+        projectAbsoluteUrl: 'https://www.vtindustries.com/',
+        projectInfoTitle: 'Vtindustries Information',
+        projectInfo: 'VT Industries Inc is a leading source of quality commercial & residential wood doors, laminate countertops, butcher block countertops, and other crafts with beautiful design possibilities.',
+        projectImages: [
+            'v1655926626/webdoc/img/portfolio/vtindustries/vtindustries-1.png', 'v1655926625/webdoc/img/portfolio/vtindustries/vtindustries-2.png',
+            'v1655926628/webdoc/img/portfolio/vtindustries/vtindustries-3.png', 'v1655926633/webdoc/img/portfolio/vtindustries/vtindustries-4.png',
+            'v1655926627/webdoc/img/portfolio/vtindustries/vtindustries-5.png'
+        ]
+    },
+];
